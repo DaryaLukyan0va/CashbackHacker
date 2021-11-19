d10108f3555a361103154ffe5896590e0a344452
Daria Lukyanova <luk.dyr@yandex.ru>
2021-11-19 12:54:20 +0300

    commit branch testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,17 +1,41 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.testng.annotations.Test
     public void giveZeroReturnThousand() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void give999ReturnOne() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void give1010Return990() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1010;
+        int actual = service.remain(amount);
+        int expected = 990;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void giveOneReturn999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
     }
 }
